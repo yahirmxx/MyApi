680119aa4465e43dd3b47d67a5d5f27760295179
Yahir Morfin <yalanmorfin@gmail.com>
2023-11-02 02:05:01 -0700

    Push de api 2

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -6,7 +6,7 @@ import { verifyToken } from "../utils/jwt.handle";
 const checkJwt = (req: RequestExt, res: Response, next: NextFunction) => {
   try {
     const jwtByUser = req.headers.authorization || "";
-    const jwt = jwtByUser.split(" ").pop(); // 11111
+    const jwt = jwtByUser.split(" ").pop(); 
     const isUser = verifyToken(`${jwt}`) as { id: string };
     if (!isUser) {
       res.status(401);
